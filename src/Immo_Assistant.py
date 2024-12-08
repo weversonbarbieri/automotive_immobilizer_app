8f4ea65ce93ad44bfcb21cdfc537d43e0740291e
weversonbarbieri <weverson.barbieri@gmail.com>
2024-12-08 13:41:03 -0300

    remove .pkl files and convert to .xlsx format

--- src/Immo_Assistant.py
+++ src/Immo_Assistant.py
@@ -1,46 +1,60 @@
 import streamlit as st
 import pandas as pd
+import requests
+
+
+
+# Get the url containing the main dataframe from the remote repository
+url_df = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/data/year_make_model_df.xlsx'
+# Requests the HTTP from the GitHub URL
+df_response = requests.get(url_df)
+# Stores the url content w/ the dataframe
+df_content = df_response.content
 
 # Load the DataFrame and save it in the variable df.
-df = pd.read_excel('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\data\year_make_model_df.xlsx')
+df = pd.read_excel(df_content)
 
 # Converting all values in column Model to string, as some of models are numbers, for instance BMWs 318, 328, 525
 df = df.astype({'Model':'string'})
 
+# Store the urls in a variable
+url_acura_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_acura_updated.pkl'
+url_audi_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_audi_updated.pkl'
+url_bmw_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_bmw_updated.pkl'
+url_buick_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_buick.pkl'
+url_cadillac_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_cadillac.pkl'
+url_chevrolet_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_chevy_filtered.pkl'
+url_chrysler_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_chrysler_updated.pkl'
+url_dodge_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_dodge_updated.pkl'
+url_fiat_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_fiat_updated.pkl'
+url_ford_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_ford_new.pkl'
+url_gmc_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_gmc.pkl'
+url_honda_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_honda_updated.pkl'
+url_hummer_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_hummer.pkl'
+url_infiniti_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_infiniti_up_to_dated.pkl'
+url_jaguar_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_jaguar_up_to_dated.pkl'
+url_jeep_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_jeep_updated.pkl'
+url_land_rover_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_land_rover_updated.pkl'
+url_lexus_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_lexus_updated.pkl'
+url_lincoln_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_lincoln_updated.pkl'
+url_mazda_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_mazda_updated.pkl'
+url_mercury_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_mercury_updated.pkl'
+url_mini_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_mini_updated.pkl'
+url_mitsubishi = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_mitsubishi_upddated.pkl'
+url_nissan_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_nissan_updated.pkl'
+url_oldsmobile_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_oldsmobile.pkl'
+url_plymouth_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_plymouth_updated.pkl'
+url_pontiac_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_pontiac.pkl'
+url_rolls_royce_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_rolls_royce_updated.pkl'
+url_saturn_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_saturn.pkl'
+url_scion_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_scion_updated.pkl'
+url_subaru_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_subaru_updated.pkl'
+url_toyota_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_toyota_updated.pkl'
+url_vw_pkl = 'https://github.com/weversonbarbieri/immobilizer_assistant.app/raw/main/dataframe_each_make_pkl_file/df_vw_updated.pkl' 
+
+
 # Load the dataframes of makes: Ford, Mercury, Lincoln, Mazda, Chevrolet, Pontiac, Cadillac, Buick, Oldsmobile, GMC, Saturn, Hummer
-df_ford_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_ford_new.pkl')
-df_lincoln_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_lincoln_updated.pkl')
-df_mercury_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_mercury_updated.pkl')
-df_mazda_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_mazda_updated.pkl')
-df_chevrolet_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_chevy_filtered.pkl') 
-df_pontiac_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_pontiac.pkl')
-df_cadillac_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_cadillac.pkl')
-df_buick_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_buick.pkl')
-df_oldsmobile_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_oldsmobile.pkl')
-df_gmc_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_gmc.pkl')
-df_saturn_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_saturn.pkl')
-df_hummer_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_hummer.pkl')
-df_land_rover_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\\Python\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_land_rover_updated.pkl')
-df_mini_st = pd.read_pickle('C:\\Language_Projects\\Language_Projects\Python\\Flagship_1\\Immo_Assistant.app\\dataframe_each_make_pkl_file\\df_mini_updated.pkl')
-df_acura_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_acura_updated.pkl')
-df_audi_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_audi_updated.pkl')
-df_bmw_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_bmw_updated.pkl')
-df_chrysler_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_chrysler_updated.pkl')
-df_dodge_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_fiat_updated.pkl')
-df_fiat_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_honda_updated.pkl')
-df_honda_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_honda_updated.pkl')
-df_infiniti_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_infiniti_up_to_dated.pkl')
-df_jaguar_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_jaguar_up_to_dated.pkl')
-df_jeep_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_jeep_updated.pkl')
-df_lexus_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_lexus_updated.pkl')
-df_mitsubishi_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_mitsubishi_upddated.pkl')
-df_nissan_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_nissan_updated.pkl')
-df_plymouth_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_plymouth_updated.pkl')
-df_rolls_royce_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_rolls_royce_updated.pkl')
-df_scion_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_scion_updated.pkl')
-df_subaru_dt = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_subaru_updated.pkl')
-df_toyota_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_toyota_updated.pkl')
-df_vw_st = pd.read_pickle('C:\Language_Projects\Language_Projects\Python\Flagship_1\Immo_Assistant.app\dataframe_each_make_pkl_file\df_vw_updated.pkl')
+
 
 # Create a dictionary with the dataframes from each make
 dataframes_each_make = {
