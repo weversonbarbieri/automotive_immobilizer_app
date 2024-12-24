037697106d99e990cb90930f36bd6fd924695713
weversonbarbieri <weverson.barbieri@gmail.com>
2024-12-24 16:21:30 -0300

    commit modifications done on .py file

--- src/Immo_Assistant.py
+++ src/Immo_Assistant.py
@@ -329,25 +329,25 @@ else:
             convert_raw_url_to_download_file(ford_parameter_reset_bcfg_url, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
         # If 'Type B' is found in 'PATS Type' and 'Parameter Reset Required' in 'ParameterReset', create the download button for Ford Parameter Reset BCFG
         elif 'Type B' in row['PATS Type'] and 'Parameter Reset Required' in row['ParameterReset']:
-            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
+            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_url, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
         # If 'Type C' is found in 'PATS Type' and 'Parameter Reset Required' in 'ParameterReset', create the download button for Ford Parameter Reset C
         elif 'Type C' in row['PATS Type'] and 'Parameter Reset Required' in row['ParameterReset']:
             convert_raw_url_to_download_file(ford_parameter_reset_c_url, ford_parameter_reset_c_message, ford_parameter_reset_c_button_label, ford_parameter_reset_c_file_name)
         # If 'Type C' is found in 'PATS Type' and 'Parameter Not Reset Required' in 'ParameterReset', create the download button for Ford PATS Type A
         elif 'Type C' in row['PATS Type'] and 'Parameter Not Reset Required' in row['ParameterReset']:
-            convert_raw_url_to_download_file(ford_pats_a_button_label, ford_pats_a_message, ford_pats_a_button_label, ford_pats_a_file_name)
+            convert_raw_url_to_download_file(ford_pats_a_url, ford_pats_a_message, ford_pats_a_button_label, ford_pats_a_file_name)
         # If 'Type E' is found in 'PATS Type' and 'Parameter Reset Required' in 'ParameterReset', create the download button for Ford Parameter Reset BCFG
         elif 'Type E' in row['PATS Type'] and 'Parameter Reset Required' in row['ParameterReset']:
-            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
+            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_url, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
         # If 'Type E' is found in 'PATS Type' and 'Parameter Not Reset Required' in 'ParameterReset', create the download button for Ford PATS Type E
         elif 'Type E' in row['PATS Type'] and 'Parameter Not Reset Required' in row['ParameterReset']:
             convert_raw_url_to_download_file(ford_pats_e_url, ford_pats_e_message, ford_pats_e_button_label, ford_pats_e_file_name)
         # If 'Type F' is found in 'PATS Type' and 'Parameter Reset Required' in 'ParameterReset', create the download button for Ford Parameter Reset BCFG
         elif 'Type F' in row['PATS Type'] and 'Parameter Reset Required' in row['ParameterReset']:
-            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
+            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_url, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
         # If 'Type G' is found in 'PATS Type' and 'Parameter Reset Required' in 'ParameterReset', create the download button for Ford Parameter Reset BCFG
         elif 'Type G' in row['PATS Type'] and 'Parameter Reset Required' in row['ParameterReset']:
-            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
+            convert_raw_url_to_download_file(ford_parameter_reset_bcfg_url, ford_parameter_reset_bcfg_message, ford_parameter_reset_bcfg_button_label, ford_parameter_reset_bcfg_file_name)
 
 
 
